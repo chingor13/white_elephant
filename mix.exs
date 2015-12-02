5d7f85508e566c5e7b1cdd7ccc03a01c4d152082
Kyle Rippey <kylerippey@gmail.com>
2015-12-01 22:54:50 -0800

    Added exrm dep

--- mix.exs
+++ mix.exs
@@ -35,7 +35,8 @@ defmodule WhiteElephant.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:exrm, "~> 0.18.1"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
