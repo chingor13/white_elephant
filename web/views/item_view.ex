033ca415cc9c451161d4f66938fd40ecdf7dd40f
Jeff Ching <ching.jeff@gmail.com>
2015-12-02 10:58:07 -0800

    more item view as json

--- web/views/item_view.ex
+++ web/views/item_view.ex
@@ -1,7 +1,16 @@
 defmodule WhiteElephant.ItemView do
   use WhiteElephant.Web, :view
 
+  def render("index.json", %{items: items}) do
+    items
+      |> Enum.map(&as_json/1)
+  end
+
   def render("item.json", %{item: item}) do
+    as_json(item)
+  end
+
+  defp as_json(item) do
     %{id: item.id, name: item.name, steals: item.steals}
   end
 end
