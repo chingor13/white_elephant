4b62547a764bc98864632ade951068fd86933b79
Jeff Ching <ching.jeff@gmail.com>
2015-12-05 14:00:22 -0800

    production configs

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :white_elephant, WhiteElephant.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [host: "we.chingr.com", port: 8888],
+  cache_static_manifest: "priv/static/manifest.json",
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
