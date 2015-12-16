0fac84d94a5c186d4bc8c6b119f2c882f2c40f80
Jeff Ching <ching.jeff@gmail.com>
2015-12-16 11:27:37 -0800

    version bump v0.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule WhiteElephant.Mixfile do
 
   def project do
     [app: :white_elephant,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
