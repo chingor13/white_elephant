ec6b76e68ea3bef921f087e7d5cf01d361d01202
Jeff Ching <ching.jeff@gmail.com>
2016-07-17 18:36:41 -0700

    fix Date.now -> Timex.now

--- web/controllers/game_controller.ex
+++ web/controllers/game_controller.ex
@@ -70,6 +70,6 @@ defmodule WhiteElephant.GameController do
 
   def now(_conn) do
     # This doesn't appear to take dst into account
-    Date.now |> DateFormat.format("%Y-%m-%d", :strftime) |> elem(1)
+    Timex.now |> Timex.format("%Y-%m-%d", :strftime) |> elem(1)
   end
 end
