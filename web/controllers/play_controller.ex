b1d16c82b7d319869a9f29917e099f4fbad1c947
Gavin Elster <gavin@avvo.com>
2016-12-15 15:24:01 -0800

    Create play layout

--- web/controllers/play_controller.ex
+++ web/controllers/play_controller.ex
@@ -3,6 +3,8 @@ defmodule WhiteElephant.PlayController do
 
   alias WhiteElephant.Game
 
+  plug :put_layout, "play.html"
+
   def play(conn, %{"game_code" => game_code}) do
     game = Repo.one!(Game |> Game.by_code(game_code))
       |> Repo.preload(:items)
