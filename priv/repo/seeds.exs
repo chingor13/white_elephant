9c862df98e372b987bfe355ded7034598990aa9e
Jeff Ching <ching.jeff@gmail.com>
2017-10-03 08:12:07 -0700

    Fix seeds and endpoint init

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,6 +10,6 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-WhiteElephant.Repo.insert!(%WhiteElephant.Game{id: 1, name: "2015 Holiday Party", max_steals: 3, date: "2015-12-05"})
+WhiteElephant.Repo.insert!(%WhiteElephant.Game{id: 1, name: "2015 Holiday Party", max_steals: 3, date: ~D[2015-12-05]})
 WhiteElephant.Repo.insert!(%WhiteElephant.Item{game_id: 1, name: "Pyrex Mixing Bowls", steals: 1})
 WhiteElephant.Repo.insert!(%WhiteElephant.Item{game_id: 1, name: "Shower Mat", steals: 2})
